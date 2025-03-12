7c7f9ed6ce49630eae0a7afc9c80a684c18b016b
Aaron Fury <afirouz@gmail.com>
2025-03-12 09:34:31 -0400

    Version update

--- simplecal/simplecal-block/build/index.asset.php
+++ simplecal/simplecal-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'd6ded1e82e441231ecf1');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '2061eead32a177e9c989');
